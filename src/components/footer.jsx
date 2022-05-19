9b42e7eb6acaac6cab37d6010e4bfa46050d57d9
kireau <37921752+kireau@users.noreply.github.com>
2022-05-19 23:36:35 +0700

    w

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import "../styles/footer.css";
 
 const footer = () => {
-  return <div className="footer_container"></div>;
+  const [aboutVisible, setAboutvisible] = useState(false)
+  return <div className="footer_container">
+    <button onClick={() => { setAboutvisible(!aboutVisible) }}>О нас</button>
+    {aboutVisible && <div>
+      Champion-Pizza доставляет вкуснейшую пиццу в Кызыле. Наши профессионалы стараются для вас и готовят каждую пиццу с любовью, чтобы наш клиент был сыт и доволен.
+      Наша команда работает с 11:00 до 23:00 и осуществляет доставку по всему Кызылу.
+    </div>} 
+  </div>;
 };
 
 export default footer;
