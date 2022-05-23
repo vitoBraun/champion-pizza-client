2383b6237a373526a7fd23e97d28de3a739fc4d3
Vitlaii Braun <vitossvx@gmail.com>
2022-05-23 10:04:13 +0300

    work in progress

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,27 +9,29 @@ const footer = () => {
           <div className="col-sm-12 col-md-6">
             <h6>О нас</h6>
             <p className="text-justify">Champion-Pizza доставляет вкуснейшую пиццу в Кызыле. Наши профессионалы стараются для вас и готовят каждую пиццу с любовью, чтобы наш клиент был сыт и доволен.
-
               Наша команда работает с 11:00 до 23:00 и осуществляет доставку по всему Кызылу..</p>
           </div>
-
           <div className="col-xs-6 col-md-3">
             <h6>Контакты</h6>
             <p className="text-justify">
               Адрес:<br />
-              г. Кызыл, Ленина 64<br />
+              г. Кызыл, Ленина 64<br /><br />
               Номера телефонов:<br />
-              6-18-18<br />
-              8-923-397-18-18<br />
-              8-923-389-1932</p>
+              <ul>
+                <li> <a href="tel:+73942261818" > 6-18-18</a>
+                </li>
+                <li> <a href="tel:+7-923-397-18-18" >+7-923-397-18-18</a>
+                </li>
+                <li> <a href="tel:+7-923-389-19-32" >+7-923-389-19-32</a>
+                </li>
+              </ul>
+            </p>
           </div>
-
           <div className="col-xs-6 col-md-3">
             <h6>Доставка и оплата</h6>
             <p className="text-justify">
-              Доставка:
               Минимальная сумма заказа для доставки 500₽<br />
-              Доставка по всему городу<br />
+              Доставка по всему городу<br /><br />
               Варианты оплаты:<br />
               <ul>
                 <li>Наличными курьеру
@@ -44,15 +46,13 @@ const footer = () => {
       </div>
       <div className="container">
         <div className="row">
-          <div className="col-md-8 col-sm-6 col-xs-12">
-            <p className="copyright-text">© 2021 Champion-Pizza
-            </p>
-          </div>
-
-          <div className="col-md-4 col-sm-6 col-xs-12">
-            <ul className="social-icons">
-              <li><a href="https://vk.com/champion_pizza17" target="_blank" rel="noopener noreferrer"><img alt="vk" src="/template/img/VK.com-logo.png" height="27px" /></a></li>
-            </ul>
+          <div className="col-md-20">
+            © 2021 Champion-Pizza
+            <div className="col-md-4 socials">
+              <ul className="social-icons">
+                <li><a href="https://vk.com/champion_pizza17" target="_blank" rel="noopener noreferrer"><img alt="vk" src="/template/img/VK.com-logo.png" height="27px" /></a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
